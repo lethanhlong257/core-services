463674a2dabd9852c58f1a512e83b397d52278d9
longle <longtle@kobiton.com>
2021-09-25 17:22:54 +0700

    update variable

--- config.js
+++ config.js
@@ -1,24 +1,24 @@
 const dialect = 'postgres'
 
-const username = process.env.KOBITON_DB_USERNAME || null
-const password = process.env.KOBITON_DB_PASSWORD || null
-const host = process.env.KOBITON_DB_HOST || 'localhost'
-const port = process.env.KOBITON_DB_PORT || 5432
-const database = process.env.KOBITON_DB_NAME || 'kobiton'
+const username = process.env.DB_USERNAME || null
+const password = process.env.DB_PASSWORD || null
+const host = process.env.DB_HOST || 'localhost'
+const port = process.env.DB_PORT || 5432
+const database = process.env.DB_NAME || 'kobiton'
 
 /**
  * These params are optional
  */
-const logging = process.env.KOBITON_DB_LOGGING == true
+const logging = process.env.DB_LOGGING == true
 // Since the size of Postgresql on test env is t2,
 // We need to set the db pool limit to a small number
 // https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/AuroraMySQL.Managing.Performance.html
-const poolMax = Number(process.env.KOBITON_DB_POOL_MAX) || 10
-const poolMin = Number(process.env.KOBITON_DB_POOL_MIN) || 5
-const poolIdle = Number(process.env.KOBITON_DB_POOL_IDLE_MS) || 10000
-const poolAcquire = Number(process.env.KOBITON_DB_POOL_ACQUIRE_MS) || 60000
-const poolEvict = Number(process.env.KOBITON_DB_POOL_EVICT_MS) || 10000
-const statementTimeout = Number(process.env.KOBITON_DB_STATEMENT_TIMEOUT) || 0
+const poolMax = Number(process.env.DB_POOL_MAX) || 10
+const poolMin = Number(process.env.DB_POOL_MIN) || 5
+const poolIdle = Number(process.env.DB_POOL_IDLE_MS) || 10000
+const poolAcquire = Number(process.env.DB_POOL_ACQUIRE_MS) || 60000
+const poolEvict = Number(process.env.DB_POOL_EVICT_MS) || 10000
+const statementTimeout = Number(process.env.DB_STATEMENT_TIMEOUT) || 0
 
 const config = {
   database,
