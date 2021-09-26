43aae245825b54b99253d557b95a31ff49daefd9
longle <longtle@kobiton.com>
2021-09-27 00:49:10 +0700

    add table activities

---
+++ db/migrations/20210926174614-create-activity.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Activities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      action: {
+        type: Sequelize.STRING
+      },
+      data: {
+        type: Sequelize.JSONB
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Activities');
+  }
+};
